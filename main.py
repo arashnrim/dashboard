a1b9e2411b59ecd7b9773e2890a99fd7058d929a
Arash <47273556+arashnrim@users.noreply.github.com>
2021-01-22 13:14:10 +0800

    🚸 Introduces spacings and overwrites to outputs

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from configparser import ConfigParser
 from datetime import datetime
+from time import sleep
 
 from styles import Style
 
@@ -47,26 +48,29 @@ while True:
     request = input(
         "Enter a number from 1 to {activityCount} or enter q to quit: ".format(activityCount=len(activities)))
 
-    if not (request in "12345q"):
-        showPrompt = False
+    if request not in "12345q" or request == "":
         print("The input was not valid; we'll try this again.")
+        sleep(1.5)
+        os.system('cls' if os.name == 'nt' else "printf '\033c\n'")
         continue
 
     if request == "1":
         # noinspection PyUnresolvedReferences
         import news
         os.system('cls' if os.name == 'nt' else "printf '\033c\n'")
-        continue
     elif request == "2":
         # noinspection PyUnresolvedReferences
         import weather
         os.system('cls' if os.name == 'nt' else "printf '\033c\n'")
-        continue
     elif request == "3":
-        pass  # TODO: Implement location feature
+        os.system('cls' if os.name == 'nt' else "printf '\033c\n'")
+        continue  # TODO: Implement location feature
     elif request == "4":
-        pass  # TODO: Implement calculations feature
+        os.system('cls' if os.name == 'nt' else "printf '\033c\n'")
+        continue  # TODO: Implement calculations feature
     elif request == "5":
-        pass  # TODO: Implement radio feature
+        os.system('cls' if os.name == 'nt' else "printf '\033c\n'")
+        continue  # TODO: Implement radio feature
     elif request == "q":
         break
+    continue
