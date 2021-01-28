632536a21ff35d10df04bb95b5bf6b27915aa66a
Arash <47273556+arashnrim@users.noreply.github.com>
2021-01-29 00:09:09 +0800

    Squashed commit of the following:
    commit 65c6f39ed7ffed8b68c0d6f6d844591b5706e6ce
    Author: Arash <47273556+arashnrim@users.noreply.github.com>
    Date:   Fri Jan 29 00:01:29 2021 +0800
    
        📝 Improve formatting of tofix.txt file
    
    commit 5afb7412c02a23fb2069a6343f1b2bb17ec2d342
    Author: Arash <47273556+arashnrim@users.noreply.github.com>
    Date:   Thu Jan 28 23:55:54 2021 +0800
    
        🔥🚚 Refactor project files
    
    commit 3cb68f4efb85cdd872ce275aa78647172fa0680f
    Merge: a203d78 72c7f64
    Author: way-daaaa <67576621+way-daaaa@users.noreply.github.com>
    Date:   Thu Jan 28 23:26:18 2021 +0800
    
        Merge pull request #9 from arashnrim/todo
    
        Add to-do feature
    
    commit ba2b42e1709cd2b0c1aeca964376d7c8a3a14f2d
    Author: Arash <47273556+arashnrim@users.noreply.github.com>
    Date:   Thu Jan 28 22:17:29 2021 +0800
    
        💬 Update text literals
    
    commit 19799fb565ce01c066b3b83cc18d0cd312db23f2
    Author: Arash <47273556+arashnrim@users.noreply.github.com>
    Date:   Thu Jan 28 22:00:55 2021 +0800
    
        ✨ Add deleting feature
    
    commit f136ef3c6c94dd1144a481eb7457473a2626c1ca
    Author: Arash <47273556+arashnrim@users.noreply.github.com>
    Date:   Thu Jan 28 21:58:39 2021 +0800
    
        ✨ Add details feature
    
    commit c76c5d03da45b952dca7082b1a8e2fd8f0cf2d60
    Author: Arash <47273556+arashnrim@users.noreply.github.com>
    Date:   Thu Jan 28 21:54:44 2021 +0800
    
        🚸 Improve output formatting
    
    commit 30093a2bcc753276d9cb833062adb41625827ac8
    Author: Arash <47273556+arashnrim@users.noreply.github.com>
    Date:   Thu Jan 28 20:26:10 2021 +0800
    
        ✨ Add conditional outputting
    
    commit e0a577891e31668645a8f7825f43014e6e384cdb
    Author: Arash <47273556+arashnrim@users.noreply.github.com>
    Date:   Thu Jan 28 20:25:54 2021 +0800
    
        🚸 Improve output formatting
    
    commit f6601578765891634d6fe7e420a7684baf31ec3b
    Author: Arash <47273556+arashnrim@users.noreply.github.com>
    Date:   Thu Jan 28 20:06:36 2021 +0800
    
        ✨ Add prompt when no tasks are present
    
    commit 6cf99c3bb40d19d8c1c0f9807496b9a05766f3a9
    Author: Arash <47273556+arashnrim@users.noreply.github.com>
    Date:   Thu Jan 28 20:02:01 2021 +0800
    
        ✨ Add basic read and write capabilities

---
+++ todo/main.py
@@ -0,0 +1,56 @@
+import os
+
+from dateutil.parser import parse
+
+from styles import Style
+from task import addTask, parseTasks, deleteTask
+
+
+def showDetails(task, request):
+    while True:
+        print("{bold}{name}{end}".format(bold=Style.BOLD, name=task["name"], end=Style.END))
+        print("Due on {}".format(parse(task["due"]).strftime("%-d %b %Y")))
+        result = input("\nType x to complete/delete, b to go back: ")
+
+        if result == "x":
+            deleteTask(request)
+            break
+        elif result == "b":
+            break
+
+
+os.system('cls' if os.name == 'nt' else "printf '\033c\n'")
+print("{}To-do{}".format(Style.BOLD, Style.END))
+
+while True:
+    if os.path.exists("{}/data.csv".format(os.path.dirname(os.path.abspath(__file__)))):
+        tasks = parseTasks()
+
+    if not (os.path.exists("{}/data.csv".format(os.path.dirname(os.path.abspath(__file__))))):
+        print("{yellow}!{end} There are no tasks.".format(yellow=Style.YELLOW, end=Style.END))
+        request_message = "\n{blue}!{end} {bold}Navigate your to-dos.{end} Type a to add a task, q to exit: ".format(
+            blue=Style.BLUE, end=Style.END, bold=Style.BOLD)
+    else:
+        if len(tasks) == 0:
+            print("{yellow}!{end} There are no tasks.".format(yellow=Style.YELLOW, end=Style.END))
+            request_message = "\n{blue}!{end} {bold}Navigate your to-dos.{end} Type a to add a task, q to exit: ".format(
+                blue=Style.BLUE, end=Style.END, bold=Style.BOLD)
+        else:
+            request_message = "\n{blue}!{end} {bold}Navigate your to-dos.{end} Type the task number to view a task, " \
+                              "a to add a task, q to exit: ".format(blue=Style.BLUE, end=Style.END, bold=Style.BOLD)
+
+    request = input(request_message)
+
+    try:
+        request = int(request)
+    except ValueError:
+        if request == "a":
+            os.system('cls' if os.name == 'nt' else "printf '\033c\n'")
+            addTask()
+            os.system('cls' if os.name == 'nt' else "printf '\033c\n'")
+        if request == "q":
+            break
+    else:
+        os.system('cls' if os.name == 'nt' else "printf '\033c\n'")
+        showDetails(tasks[request - 1], request - 1)
+        os.system('cls' if os.name == 'nt' else "printf '\033c\n'")
